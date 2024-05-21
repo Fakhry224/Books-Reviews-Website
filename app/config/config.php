853ab12999897d2fc42c4368717406227dc5624b
Fakhry224 <112813649+Fakhry224@users.noreply.github.com>
2024-05-21 20:26:04 +0700

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -7,5 +7,5 @@ define('BASEURL', 'http://localhost/pemweb/projek2/public');
 // DB
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'rahasiapengguna');
+define('DB_PASS', '');
 define('DB_NAME', 'books_review');
