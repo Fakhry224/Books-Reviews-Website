d5d3cc4425963ba66f7301e2c2e1ba25e4ca5bc7
Fakhry224 <112813649+Fakhry224@users.noreply.github.com>
2024-05-22 10:14:23 +0700

    Update and rename BookDetail.php to BookController.php

--- app/controllers/BookController.php
+++ app/controllers/BookController.php
@@ -1,6 +1,6 @@
 <?php
 
-class BookDetail extends Controller {
+class BookController extends Controller {
     private $metadata = [
         'css' => '/css/detail_style',
         'title' => 'Book Detail Page'
@@ -8,16 +8,15 @@ class BookDetail extends Controller {
 
     public function index() {
         $book_model = $this->model('BookModel');
-
+        
         if (isset($_POST['book_id'])) {
             $book_id = $_POST['book_id'];
             $_SESSION['book_id'] = $book_id;
-            // echo 'Ini Book ID : ' . $book_id;
         } 
 
         $data = [
             'detail_book' => $book_model->getBookDetail($_SESSION['book_id']),
-            'comment' => $book_model->getAllComment()
+            'comment' => $book_model->getAllComment($_SESSION['book_id'])
         ];
 
         $this->view('template/header', $this->metadata);
@@ -29,24 +28,24 @@ class BookDetail extends Controller {
          if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
             $data = [
-                'book_id' => $_POST['book_id'],
-                'user_id' => $_POST['user_id'],
+                'book_id' => $_SESSION['book_id'],
+                'user_id' => $_SESSION['user_id'],
                 'comment' => trim($_POST['comment'])
             ];
 
             if (empty($data['comment'])) {
                 Flasher::setFlash('Failed Insert Comment', 'Cannot Insert Empty Comment', 'danger');
-                header('Location: ' . BASEURL . '/BookDetail');
+                header('Location: ' . BASEURL . '/BookController');
                 exit;
             }
 
             if ($this->model('BookModel')->insertComment($data) > 0) {
                 Flasher::setFlash('Successfuly Insert Comment', 'Success', 'success');
-                header('Location: ' . BASEURL . '/BookDetail');
+                header('Location: ' . BASEURL . '/BookController');
                 exit;
             } else {
                 Flasher::setFlash('Error Insert Comment', 'Failed', 'danger');
-                header('Location: ' . BASEURL . '/BookDetail');
+                header('Location: ' . BASEURL . '/BookController');
                 exit;
             }
         }
